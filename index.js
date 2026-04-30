a68f37fa00f6a05b0c1c81efb850a0718f218277
innovativecoder1267 <aaravchaprana1627@gmail.com>
2026-04-30 16:33:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app=express();
+
+app.use(express.json());
+
+const port=3000;
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
+app.get("/",(req,res)=>{
+    res.send("Hello World ARE HAAAAAAAAAAA");
+})
